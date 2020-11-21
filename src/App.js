0fc968bda4ea34c73a2bfb46919901683e198399
Nujood <shawar.nujood@gmail.com>
2020-11-21 23:03:37 +0200

    standard handling change and submit from

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+const {useState}= React
+
+const App =() => {
+
+const [todos, setTodos] = useState([])
+
+  
+  const handleChange = (e) => {
+    setTodos(e.target.value)
+  console.log('handle change', todos);  
+  }  
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
+        <form onSubmit={handleSubmit}>
+          <input type='text' value={todos} onChange={handleChange}  placeholder="enter your task  over here"/>
+          <button>ADDD it</button>
+        </form>
+
       </header>
     </div>
   );
